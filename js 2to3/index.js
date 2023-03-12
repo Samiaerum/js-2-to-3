9691b776bdea7cf5e1b334d05b1c9b234abcdd66
Samiaerum <121792974+Samiaerum@users.noreply.github.com>
2023-03-12 14:18:47 +0500

    Add files via upload

---
+++ js 2to3/index.js
@@ -0,0 +1,101 @@
+// chap 2 question 1
+
+var firstname="samia";
+ var secondname="samia erum"
+
+/////////////////////////////////////////////////////////////////////////////
+
+// question 2
+var message="hello world"
+alert(message);
+
+// question 3
+
+var name="samia"
+var age="18 year old"
+var admited="enroll in web development and mobile applicaton course"
+alert(name);
+alert( age);
+alert(admited);
+
+
+/////////////////////////////////////////////////////////////////////////
+
+// question 4
+
+var abc="pizza \n Pizz \n Piz \n Pi \n P"
+alert(abc);
+
+
+
+////////////////////////////////////////////////////////////////////////////
+
+// question 5
+
+var x =" my email address is "
+ var y ="example@example.com"
+ var z =x+y;
+alert(z);
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+/////question 6
+
+var xyz =" i m trying to learn from the book A smarter way to learn JavaScript"
+alert(xyz);
+
+
+
+////////////////////////////////////////////////////////////////////////////
+
+//  question 7
+
+//  var myname ="yah! i can write html content through js"
+ 
+//  document.write="myname"
+
+
+
+
+// question 8
+
+//////////////////////////////////////////////////////////////////////////////
+var efg ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(efg);
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+// chap 3 question 1
+
+var age1 ="                                             i m 19 years old                                         "
+alert(age1);
+
+
+
+/////////////////////////////////////////////////////////////////
+
+// question 2
+
+var visitor=14;
+alert("you have visited this site "+14+"times")
+
+
+///////////////////////////////////////////////////////////////
+ 
+// question 3
+
+// var birthyear=2003
+// document.write("you have" "+birthyear+"" years old");
+
+
+
+//////////////////////////////////////////////////////////////////////
+//question 4
+
+var name="samia";
+var quantity=6;
+var product="t-shirt";
+document.write(name+" ordered "+quantity+" " +product+ "  from xyz clothing store");
